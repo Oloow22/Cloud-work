03252f709daa910ec221979320b612bd0656904f
Audrey Nakate <audreynakate222@gmail.com>
2023-05-14 20:30:44 +0300

    Trying to get static files to show up

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -119,5 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 ACCOUNT_LOGOUT_REDIRECT_URL = "/blog"
