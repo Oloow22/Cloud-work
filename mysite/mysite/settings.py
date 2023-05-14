b70ee1e507da954c3f120d6bdac6895fa6edafb6
Audrey Nakate <audreynakate222@gmail.com>
2023-05-14 21:45:46 +0300

    Still working on the static files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'blog/static'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
